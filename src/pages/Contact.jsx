d59f90d4d964d7f587d4ded397edde3d0b2a58e0
HarishRaajendar R <harishraajendar2006@gmail.com>
2026-02-02 14:33:29 +0530

    contactchg

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import { FaWhatsapp } from "react-icons/fa";
 
 const pageVariants = {
   initial: { y: 50, opacity: 0 },
@@ -15,7 +16,24 @@ const Contact = () => {
       exit="exit"
       transition={{ duration: 0.5 }}
     >
-      <div></div>
+      <div className="contact">
+        <img src="/images/profileicon.png" alt="profile" />
+        <div className="contact-content">
+          <h1>
+            <span style={{ color: "whitesmoke" }}>H</span>ARISH
+            <span style={{ color: "whitesmoke" }}>R</span>AAJENDAR R
+          </h1>
+          <h3>React Developer</h3>
+          <p>I build modern web applications using React.</p>
+        </div>
+      </div>
+      <div className="contact-btns">
+        <a href="">
+          <button>
+            <FaWhatsapp />
+          </button>
+        </a>
+      </div>
     </motion.div>
   );
 };
